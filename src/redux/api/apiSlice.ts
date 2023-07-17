c15f4e8ed4f805bdab8d45ed5e7cefe6ddb717cb
Shoyeb Mohammed Suny <69183694+sunywebdev@users.noreply.github.com>
2023-07-17 16:32:39 +0600

    api added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:5000",
+		baseUrl: "https://book-catalog-server-production.up.railway.app",
 	}),
 	endpoints: () => ({}),
 	tagTypes: ["books", "comment"],

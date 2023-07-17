cee974d04fb64874a86fe46ec19d755566e5c6ea
Shoyeb Mohammed Suny <69183694+sunywebdev@users.noreply.github.com>
2023-07-17 22:42:37 +0600

    signin, signup

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -34,30 +34,30 @@ const SignIn = () => {
 
 	return (
 		<form onSubmit={handleSignIn}>
-			<div className='container'>
-				<div className='p-5 md:p-8 lg:p-16'>
+			<div className='container text-center'>
+				<div className='p-5 md:p-8 lg:p-16 w-2/3 mx-auto'>
 					<div className='mb-5'>
 						<h3 className='text-xl font-semibold'>SignIn</h3>
 						<p>Login to your account.</p>
 					</div>
-					<div className='form-control w-full max-w-xs'>
+					<div className='form-control w-full'>
 						<label className='label'>
 							<span className='label-text'>Email</span>
 						</label>
 						<input
 							type='text'
 							name='email'
-							className='input input-sm input-bordered w-full max-w-xs'
+							className='input input-sm input-bordered w-full'
 						/>
 					</div>
-					<div className='form-control w-full max-w-xs'>
+					<div className='form-control w-full'>
 						<label className='label'>
 							<span className='label-text'>Password</span>
 						</label>
 						<input
 							type='password'
 							name='password'
-							className='input input-sm input-bordered w-full max-w-xs'
+							className='input input-sm input-bordered w-full'
 						/>
 					</div>
 					<button className='btn btn-primary btn-sm text-xs px-16 mt-5'>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -31,31 +31,31 @@ const SignUp = () => {
 
 	return (
 		<form onSubmit={handleSignUp}>
-			<div className='container'>
-				<div className='p-5 md:p-8 lg:p-16'>
+			<div className='container text-center'>
+				<div className='p-5 md:p-8 lg:p-16 w-2/3 mx-auto'>
 					<div className='mb-5'>
 						<h3 className='text-xl font-semibold'>SignUp</h3>
 						<p>Register a new account.</p>
 					</div>
 
-					<div className='form-control w-full max-w-xs'>
+					<div className='form-control w-full'>
 						<label className='label'>
 							<span className='label-text'>Email</span>
 						</label>
 						<input
 							type='text'
 							name='email'
-							className='input input-sm input-bordered w-full max-w-xs'
+							className='input input-sm input-bordered w-full'
 						/>
 					</div>
-					<div className='form-control w-full max-w-xs'>
+					<div className='form-control w-full'>
 						<label className='label'>
 							<span className='label-text'>Password</span>
 						</label>
 						<input
 							type='password'
 							name='password'
-							className='input input-sm input-bordered w-full max-w-xs'
+							className='input input-sm input-bordered w-full'
 						/>
 					</div>
 					<button className='btn btn-primary btn-sm text-xs px-16 mt-5'>
